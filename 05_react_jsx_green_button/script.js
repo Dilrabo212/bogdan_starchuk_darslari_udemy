b21262eda40725c31640339fb5ba8b5382732aed
dilrabo <dilraboxujamuratova@gmail.com>
2023-08-27 22:22:41 +0500

    react polniy kurs boshlanishi

---
+++ 05_react_jsx_green_button/script.js
@@ -0,0 +1,25 @@
+
+// const App = (
+//     <div className="app">
+//         <button onClick="">
+//             Click Me!
+//         </button>
+//     </div>
+
+// )
+// const container = document.getElementById('app')
+// const root = ReactDOM.createRoot(container)
+// root.render(App)
+
+
+const App = (
+    <div className="app">
+        <button onClick="">
+            Meni bosing!
+        </button>
+    </div>
+
+)
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+root.render(App)
